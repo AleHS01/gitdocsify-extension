40413136e17e137cbfd8dd540d536700326f2232
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2025-04-02 00:11:36 -0400

    feature(ui) Hover Spotlight Effect
    - Added Hover Spotlight Effect to RepoCard component
    
    Potential Improvement:
    - Add animation when they are being loaded in dashboard -> motion
    - Spotlight goes outsite of the box when getting near edge or corner

--- frontend/src/components/RepoCard.tsx
+++ frontend/src/components/RepoCard.tsx
@@ -8,16 +8,33 @@ import { Link } from "react-router-dom";
 import { Box, Text, CircleBadge, Heading } from "@primer/react";
 import { Repository } from "../types/repository";
 import { languageColors } from "../utils/languageColors";
+import { useState } from "react";
 
 type RepoCardProps = {
   repo: Repository;
 };
 
 const RepoCard: React.FC<RepoCardProps> = ({ repo }) => {
+  const [gradient, setGradient] = useState("rgba(255,255,255,0.1)");
+
   const getLanguageColor = (language: string) => {
     if (!language) return "transparent";
-
     return languageColors[language] || "transparent";
+  };
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const { left, top, width, height } =
+      e.currentTarget.getBoundingClientRect();
+    const x = ((e.clientX - left) / width) * 100;
+    const y = ((e.clientY - top) / height) * 100;
+
+    setGradient(
+      `radial-gradient(circle at ${x}% ${y}%, rgba(255, 255, 255, 0.04), transparent)`
+    );
+  };
+
+  const handleMouseLeave = () => {
+    setGradient("transparent");
   };
 
   return (
@@ -36,11 +53,16 @@ const RepoCard: React.FC<RepoCardProps> = ({ repo }) => {
           p: 3,
           textDecoration: "none",
           color: "inherit",
-          "&:hover": {
-            boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)",
-          },
           height: "220px",
+          position: "relative",
+          transition: "box-shadow 0.2s ease-in-out",
+          backgroundImage: gradient,
+          "&:hover": {
+            boxShadow: "0px 4px 15px rgba(0, 0, 0, 0.5)",
+          },
         }}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
       >
         <Box
           sx={{
